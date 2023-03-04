57c4a9c2ba8fd4a63effda27b07b8492f9150b83
bulmang <hmk113737@gmail.com>
2023-03-05 04:00:14 +0900

    Initial Commit

---
+++ doubleList.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    var result = [Int]() // result 빈 배열 선언
+    for i in numbers {
+        let doubleList = i * 2    // 배열 2배
+        result.append(doubleList)
+    }
+    return result
+}
