97e1e9c0c99a5e83efaea6b62cf567a40a599851
bulmang <hmk113737@gmail.com>
2023-03-08 01:44:47 +0900

    [배열] 중앙값 구하기

--- findingMedian.playground/Contents.swift
+++ findingMedian.playground/Contents.swift
@@ -3,7 +3,7 @@ import Foundation
 func solution(_ array:[Int]) -> Int {
     
     var sortedArray = [Int]() // 빈 배열 추가
-    var sortedNumbers = array.sorted() // 배열 오름차순 변경, 내림찬순의 경우 sorted(by:) 사용
+    var sortedNumbers = array.sorted() // 배열 오름차순 변경, 내림차순의 경우 sorted(by:) 사용
     var result: Int
     
     sortedArray.append(contentsOf: sortedNumbers) //append는 하나의 요소만 추가, append(contentsOf:)는 다른 배열의 모든 요소 추가
