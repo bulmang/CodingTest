1a13808558d106b35d9f443cb0ce2a6bcd602bd3
bulmang <hmk113737@gmail.com>
2023-03-14 08:26:05 +0900

    배열에 두배 값 넣기

--- doubleList.playground/Contents.swift
+++ doubleList.playground/Contents.swift
@@ -4,7 +4,7 @@ func solution(_ numbers:[Int]) -> [Int] {
     var result = [Int]() // result 빈 배열 선언
     for i in numbers {
         let doubleList = i * 2    // 배열 2배
-        result.append(doubleList)
+        result.append(doubleList) // 리스트 추가
     }
     return result
 }
